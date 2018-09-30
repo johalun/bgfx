7d04695422c4e0eca938beed69a001b64159ef82
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-09-30 14:38:43 +0300

    CI configuration for macOS

--- src/bgfx.cpp
+++ src/bgfx.cpp
@@ -4955,6 +4955,16 @@ BGFX_C_API void bgfx_reset(uint32_t _width, uint32_t _height, uint32_t _flags, b
 	bgfx::reset(_width, _height, _flags, bgfx::TextureFormat::Enum(_format) );
 }
 
+BGFX_C_API bgfx_encoder_s* bgfx_begin()
+{
+	return (bgfx_encoder_s*)bgfx::begin();
+}
+
+BGFX_C_API void bgfx_end(bgfx_encoder_s *_encoder)
+{
+	bgfx::end((bgfx::Encoder *)_encoder);
+}
+
 BGFX_C_API uint32_t bgfx_frame(bool _capture)
 {
 	return bgfx::frame(_capture);
